088b40958aa31c5cae892990483e1de6297dbab1
alexanderley <alexander.ley.inbox@gmail.com>
2023-06-12 16:53:45 +0200

    second commit, more functionality added

---
+++ js/game.js
@@ -0,0 +1,153 @@
+class Game {
+  constructor() {
+    this.startBtn = document.querySelector(".startGame");
+    this.playerHand = document.querySelector(".player1Container");
+    this.cardStack = document.querySelector(".activeCard");
+    this.playerOneCards = document.querySelector(".playerOneCards");
+    this.playerEnemyCards = document.querySelector(".enemyCards");
+    this.topDeckCard = document.querySelector(".topDeckCard");
+    this.fieldCards = document.querySelector(".activeCardList");
+
+    this.playerCards = [];
+    this.enemyCards = [];
+
+    // click handler
+    this.startBtn.addEventListener("click", this.startGame.bind(this));
+    this.topDeckCard.addEventListener("click", this.drawSingleCard.bind(this));
+
+    this.playerOneCards.addEventListener("click", this.playCard);
+  }
+  startGame() {
+    this.drawCards(7, "player");
+    this.drawCards(7, "enemy");
+    this.renderCardHand();
+
+    // create the fist field card
+    const firstCardData = this.randomCard(gameCards);
+    const firstCardElement = this.createCard(firstCardData);
+    console.log("firstCard", firstCardElement);
+    this.fieldCards.appendChild(firstCardElement);
+  }
+
+  renderCardHand() {
+    // render Player cards
+    playerCards.forEach((card) => {
+      const li = document.createElement("li");
+      li.classList.add("card");
+      const img = document.createElement("img");
+      img.src = card.image;
+      img.setAttribute("color", card.color);
+      img.setAttribute("number", card.number);
+      img.setAttribute("special", card.special);
+
+      li.appendChild(img);
+      this.playerOneCards.appendChild(li);
+    });
+
+    // render place holder of the enemy
+    enemyCards.forEach((card) => {
+      const li = document.createElement("li");
+      li.classList.add("card");
+      const img = document.createElement("img");
+      img.src = "assets/Deck.png";
+      img.setAttribute("color", card.color);
+      img.setAttribute("number", card.number);
+      img.setAttribute("special", card.special);
+
+      li.appendChild(img);
+      this.playerEnemyCards.appendChild(li);
+    });
+  }
+
+  // shuffleCards(cards) {
+  //   let currentIndex = cards.length,
+  //     randomIndex;
+  //   while (currentIndex !== 0) {
+  //     randomIndex = Math.floor(Math.random() * currentIndex);
+  //     currentIndex--;
+  //     [cards[currentIndex], cards[randomIndex]] = [
+  //       cards[randomIndex],
+  //       cards[currentIndex],
+  //     ];
+  //   }
+  //   return cards;
+  // }
+
+  randomCard(arr) {
+    const arrayLength = arr.length;
+    const randomIndex = Math.floor(Math.random() * arrayLength);
+    const randomElement = arr[randomIndex];
+    return randomElement;
+  }
+
+  createCard(cardData) {
+    const newCardLi = document.createElement("li");
+    newCardLi.classList.add("card");
+    const img = document.createElement("img");
+    img.src = cardData.image;
+    img.setAttribute("color", cardData.color);
+    img.setAttribute("number", cardData.number);
+    img.setAttribute("special", cardData.special);
+    newCardLi.appendChild(img);
+    return newCardLi;
+  }
+
+  drawCards(amount, player) {
+    if (player === "player") {
+      for (let i = 0; i < amount; i++) {
+        playerCards.push(this.randomCard(gameCards));
+      }
+    } else if (player === "enemy") {
+      for (let i = 0; i < amount; i++) {
+        enemyCards.push(this.randomCard(gameCards));
+      }
+    }
+    console.log("Enemy hand", enemyCards);
+    console.log("Player hand", playerCards);
+  }
+
+  // playCard(event) {
+  //   if (event.target.tagName === "IMG") {
+  //     if (event.target.color === fieldCards[0].color) {
+  //       const liElement = event.target.closest("li");
+  //       if (liElement) {
+  //         liElement.remove();
+  //       }
+  //     }
+  //   }
+  // }
+
+  playCard(event) {
+    if (event.target.tagName === "IMG") {
+      console.log("fieldCards", fieldC);
+      const liElement = event.target.closest("li");
+      if (liElement) {
+        liElement.remove();
+      }
+    }
+  }
+
+  drawSingleCard() {
+    const newCardData = this.randomCard(gameCards);
+    const newCardLi = this.createCard(newCardData);
+    this.playerCards.push(newCardData);
+    this.playerOneCards.appendChild(newCardLi);
+  }
+
+  playSingleCard() {}
+
+  pushCards() {
+    this.checkGameState();
+    this.renderCardHand();
+  }
+
+  checkGameState() {
+    if (playerCards.length === 0) {
+      console.log("You win the game");
+    } else if (enemyCards.length === 0) {
+      console.log("You lost the game");
+    }
+  }
+}
+
+const game = new Game();
